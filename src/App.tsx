e8eae1b91c1f071df5874be2c6db93386f7e402d
ryan <jianronglu@upinnov.com>
2024-11-28 18:04:11 +0800

    使用 interface 或 type 可以重用类型定义

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      {Welcome()}
-      <MyButton title="Click me" />
+      <Welcome />
+      <MyButton title="我是一个按钮" />
+      <MyButtonDisabled title="我是一个被禁用的按钮" disabled={true} />
+      <img className="avatar" alt="logo" src='https://avatars.githubusercontent.com/u/10347539?v=4' />
       </header>
     </div>
   );
@@ -21,4 +23,19 @@ function MyButton({ title }: { title: string }) {
     <button>{title}</button>
   );
 }
+
+interface MyButtonProps {
+  title: string;
+  disabled?: boolean;
+}
+
+function MyButtonDisabled({ title, disabled }: MyButtonProps) {
+  return (
+    <button disabled={disabled}>{title}</button>
+  );
+}
+
+
+
+
 export default App;
