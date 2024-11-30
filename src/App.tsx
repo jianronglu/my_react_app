e2b789dcd7d6842d087a1081aa9435efefdd3f8a
ryan <jianronglu@upinnov.com>
2024-11-30 09:44:04 +0800

    buid: 安装eslint

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundColor: 'red' ,color: 'white'}}>
       <header className="App-header">
       <Welcome />
       <MyButton title="我是一个按钮" />
       <MyButtonDisabled title="我是一个被禁用的按钮" disabled={true} />
-      <img className="avatar" alt="logo" src='https://avatars.githubusercontent.com/u/10347539?v=4' />
+      <MyImg avatarUrl='https://avatars.githubusercontent.com/u/10347539?v=4' name='logo' width={100} height={100} />
+      <AboutPage />
       </header>
     </div>
   );
@@ -32,6 +33,26 @@ interface MyButtonProps {
 function MyButtonDisabled({ title, disabled }: MyButtonProps) {
   return (
     <button disabled={disabled}>{title}</button>
+  );
+}
+
+interface MyImgProps {
+  avatarUrl: string;
+  name: string;
+  width?: number;
+  height?: number;
+}
+
+function MyImg({ avatarUrl , name, width, height}: MyImgProps){
+  return <img className="avatar" alt={name} src={avatarUrl} style={{width, height}}/>
+}
+
+function AboutPage() {//空标签-使用Fragment包裹-减少标签层级
+  return (
+    <>
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+    </>
   );
 }
 
